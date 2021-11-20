36fa264459e3ab6b5469216cc9bde542dd891a81
ZoranUTF8 <janjiczoran23@gmail.com>
2021-11-20 21:36:43 +0900

    Added form validation

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,12 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import emailjs from "emailjs-com";
-import { Form, Button, FloatingLabel } from "react-bootstrap";
+import { Form, Button, FloatingLabel, Row, Col, Alert } from "react-bootstrap";
 
 export default function Contact() {
   const form = useRef();
+  const [sentEmail, setSentEmail] = useState(false);
 
+  //* email client
   function sendEmail(e) {
     e.preventDefault();
 
@@ -24,37 +26,66 @@ export default function Contact() {
         }
       );
     form.current.reset();
+    setSentEmail(true);
   }
 
   return (
-    <Form ref={form} className="container contact " onSubmit={sendEmail}>
-      <h2>Send me a message</h2>
-      <FloatingLabel controlId="name" label="Your name" className="mb-3 ">
-        <Form.Control type="text" name="name" />
-      </FloatingLabel>
+    <div className="container">
+      <Row>
+        <Col md={6}>
+          <Form ref={form} className=" contact " onSubmit={sendEmail}>
+            <h2>Send me a message</h2>
+            <FloatingLabel controlId="name" label="Your name" className="mb-3 ">
+              <Form.Control type="text" name="name" required />
+            </FloatingLabel>
 
-      <FloatingLabel controlId="email" label="Your email" className="mb-3">
-        <Form.Control type="email" name="email" />
-      </FloatingLabel>
+            <FloatingLabel
+              controlId="email"
+              label="Your email"
+              className="mb-3"
+            >
+              <Form.Control type="email" name="email" required />
+            </FloatingLabel>
 
-      <FloatingLabel controlId="message" label="Your message">
-        <Form.Control
-          as="textarea"
-          placeholder="Leave your message here"
-          style={{ height: "20vh" }}
-          name="message"
-        />
-      </FloatingLabel>
-      <div className="d-grid gap-2 m-3">
-        <Button
-          type="submit"
-          variant="btn btn-info"
-          size="lg"
-          value="Send message"
-        >
-          Send
-        </Button>
-      </div>
-    </Form>
+            <FloatingLabel controlId="message" label="Your message">
+              <Form.Control
+                as="textarea"
+                placeholder="Leave your message here"
+                style={{ height: "20vh" }}
+                name="message"
+                required
+              />
+            </FloatingLabel>
+            <div className="d-grid gap-2 m-3">
+              <Button
+                type="submit"
+                variant="btn btn-info"
+                size="lg"
+                value="Send message"
+              >
+                Send
+              </Button>
+            </div>
+          </Form>
+        </Col>
+        <Col md={6}></Col>
+      </Row>
+      {/*  add modal for this */}
+      {sentEmail && (
+        <Alert variant="success">
+          <Alert.Heading>Hey, nice to see you</Alert.Heading>
+          <p>
+            Aww yeah, you successfully read this important alert message. This
+            example text is going to run a bit longer so that you can see how
+            spacing within an alert works with this kind of content.
+          </p>
+          <hr />
+          <p className="mb-0">
+            Whenever you need to, be sure to use margin utilities to keep things
+            nice and tidy.
+          </p>
+        </Alert>
+      )}
+    </div>
   );
 }
